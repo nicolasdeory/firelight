6e6858097b53f3203f62cc1023adea6ac75d033d
Nicolás de Ory <deorynicolas@gmail.com>
2020-04-28 22:01:36 +0200

    Added docs for lightzone

--- LedDashboardCore/LightZone.cs
+++ LedDashboardCore/LightZone.cs
@@ -1,7 +1,10 @@
 ﻿namespace LedDashboardCore
 {
+    /// <summary>
+    /// Flag used for specifying what zones the lighting frame should be applied to. If a flag isn't set, this frame won't override the color information that was present before.
+    /// </summary>
     public enum LightZone
     {
-        Keyboard, Mouse, Headset, Strip, Lightbulb
+        Keyboard, Strip, Mouse, Mousepad, Headset, Keypad, General
     }
 }
