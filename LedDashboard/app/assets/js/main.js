a903fd93bd142f8fd695c111fb321fd4d563e132
Nicolás de Ory <deorynicolas@gmail.com>
2020-07-03 20:58:24 +0200

    Added keyboard layout

---
+++ LedDashboard/app/assets/js/main.js
@@ -0,0 +1,44 @@
+﻿function setKeyboardColor(index, hex) {
+    if (index == 46) {
+        $("#k46").attr("fill", hex);
+        $("#k46-1").attr("fill", hex);
+    } else {
+        $("#k" + index).attr("fill", hex);
+    }
+}
+
+
+$(document).ready(() =>
+{
+
+    $("#tab-lights").click(function ()
+    {
+        if ($(this).hasClass("selected"))
+            return;
+        $(".tab").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#tab-games").click(function ()
+    {
+        if ($(this).hasClass("selected"))
+            return;
+        $(".tab").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    $("#tab-settings").click(function ()
+    {
+        if ($(this).hasClass("selected"))
+            return;
+        $(".tab").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+    let i = 0;
+    setInterval(() => {
+        setKeyboardColor(i % 109, "#fff");
+        i++;
+    }, 500);
+
+});
