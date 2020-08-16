c0aa7f4bc2dd7c3930dd55d49f5cec0697ab9390
Nicolás de Ory <deorynicolas@gmail.com>
2020-08-16 20:16:40 +0200

    Improved light UI colors

--- LedDashboard/app/assets/js/lights.js
+++ LedDashboard/app/assets/js/lights.js
@@ -1,4 +1,72 @@
-﻿function setKeyboardColor(index, hex) {
+﻿/**
+ * A linear interpolator for hexadecimal colors
+ * @param {String} a
+ * @param {String} b
+ * @param {Number} amount
+ * @example
+ * // returns #7F7F7F
+ * lerpColor('#000000', '#ffffff', 0.5)
+ * @returns {String}
+ */
+function lerpColor(a, b, amount) {
+
+    var ah = parseInt(a.replace(/#/g, ''), 16),
+        ar = ah >> 16, ag = ah >> 8 & 0xff, ab = ah & 0xff,
+        bh = parseInt(b.replace(/#/g, ''), 16),
+        br = bh >> 16, bg = bh >> 8 & 0xff, bb = bh & 0xff,
+        rr = ar + amount * (br - ar),
+        rg = ag + amount * (bg - ag),
+        rb = ab + amount * (bb - ab);
+
+    return '#' + ((1 << 24) + (rr << 16) + (rg << 8) + rb | 0).toString(16).slice(1);
+}
+
+function hexToHSL(H) {
+    // Convert hex to RGB first
+    let r = 0, g = 0, b = 0;
+    if (H.length == 4) {
+        r = "0x" + H[1] + H[1];
+        g = "0x" + H[2] + H[2];
+        b = "0x" + H[3] + H[3];
+    } else if (H.length == 7) {
+        r = "0x" + H[1] + H[2];
+        g = "0x" + H[3] + H[4];
+        b = "0x" + H[5] + H[6];
+    }
+    // Then to HSL
+    r /= 255;
+    g /= 255;
+    b /= 255;
+    let cmin = Math.min(r, g, b),
+        cmax = Math.max(r, g, b),
+        delta = cmax - cmin,
+        h = 0,
+        s = 0,
+        l = 0;
+
+    if (delta == 0)
+        h = 0;
+    else if (cmax == r)
+        h = ((g - b) / delta) % 6;
+    else if (cmax == g)
+        h = (b - r) / delta + 2;
+    else
+        h = (r - g) / delta + 4;
+
+    h = Math.round(h * 60);
+
+    if (h < 0)
+        h += 360;
+
+    l = (cmax + cmin) / 2;
+    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+    s = +(s * 100).toFixed(1);
+    l = +(l * 100).toFixed(1);
+
+    return [h, s, l];
+}
+
+function setKeyboardColor(index, hex) {
     if (index == 46) {
         $("#k46").attr("fill", hex);
         $("#k46-1").attr("fill", hex);
@@ -13,10 +81,13 @@ function setLedColor(prefix, index, hex) {
 
 
 function sanitizeHex(hex) {
-    if (hex == "000000")
-        hex = "rgba(255,255,255,0.05)";
+    hex = hex.toLowerCase();
+    var hsl = hexToHSL("#" + hex);
+    var luminosity = hsl[2] / 100;
+    if (luminosity < 0.2)
+        hex = lerpColor("#2D2D2D", hex, luminosity);
     else
-        hex = "#" + hex;
+        hex = "#" + hex
     return hex;
 }
 
